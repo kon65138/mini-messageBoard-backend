c8c534a7eabdba46fad217dc615e12a3a72d974a
MisterPrestoGamer <prestogamer12345@gmail.com>
2026-03-26 16:09:20 +0000

    update12

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -4,6 +4,7 @@ module.exports = {
   getMsgById: async (req, res) => {
     const { messageId } = req.params;
     const message = await db.getMsgById(messageId);
+    console.log(message);
     res.render('message', { curMsg: message });
   },
 };
